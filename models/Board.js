014a876fc895d9c7f194e4d2519c36cfe4ea2a6c
Deekshant Yadav <d33kshant@gmail.com>
2022-02-08 23:09:49 +0530

    Add banner and icon in board's model

--- models/Board.js
+++ models/Board.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 export default mongoose.model('Board', new mongoose.Schema({
 	name: String,
 	about: String,
+	icon: String,
+	banner: String,
 	created: Date,
 	creator: String,
 }))
