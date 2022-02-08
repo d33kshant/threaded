5c729637331615d693ef8d460b251562d30987da
Deekshant Yadav <d33kshant@gmail.com>
2022-02-08 22:14:46 +0530

    Create basic express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+require('dotenv').config()
+const express = require('express')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.listen(PORT, () => console.log('Sever listening on port:', PORT))
