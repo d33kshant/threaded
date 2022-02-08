66085d6b63280a0041ddd16ee61b356d08c4dea6
Deekshant Yadav <d33kshant@gmail.com>
2022-02-08 23:25:22 +0530

    Add basic echo auth route

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 
+const authRoute = require('./routes/AuthRoutes')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+app.use('/api/auth', authRoute)
 
 app.listen(PORT, () => console.log('Sever listening on port:', PORT))
