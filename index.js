dd092fc56f53bb9c6412ec78989c27b3eb2d96e4
Deekshant Yadav <d33kshant@gmail.com>
2022-02-10 15:50:52 +0530

    Add board route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 
 const authRoute = require('./routes/AuthRoutes')
 const postRoute = require('./routes/PostRoutes')
+const boardRoute = require('./routes/BoardRoutes')
 
 const PORT = process.env.PORT || 5000
 const DB_URI = process.env.DB_URI
@@ -13,6 +14,7 @@ app.use(express.json())
 
 app.use('/api/auth', authRoute)
 app.use('/api/post', postRoute)
+app.use('/api/board', boardRoute)
 
 mongoose.connect(DB_URI, (error) => {
 	if (error) return console.log("Can't connect to database.")
