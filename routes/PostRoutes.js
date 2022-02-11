78c91fc5391237e7babe479a9203e66e3ef00ac3
Deekshant Yadav <d33kshant@gmail.com>
2022-02-11 15:55:04 +0530

    Add route to like post

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -1,11 +1,10 @@
 const router = require('express').Router()
-const { getPost, getPosts, createPost } = require('../controllers/PostController')
+const { getPost, getPosts, createPost, likePost } = require('../controllers/PostController')
 const { authenticate, authorize } = require('../controllers/AuthController')
 
-router.get('/:id', authorize, getPost)
-
-router.get('/', authorize, getPosts)
-
 router.post('/create', authenticate, createPost)
+router.post('/like', authenticate, likePost)
+router.get('/:id', authorize, getPost)
+router.get('/', authorize, getPosts)
 
 module.exports = router
