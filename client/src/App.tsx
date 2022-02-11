a98bbe184dc7ca8cb727956b5994f60bdd39ae6f
Deekshant Yadav <d33kshant@gmail.com>
2022-02-11 22:40:21 +0530

    Add routing and auth context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from 'react-router-dom'
+import AuthProvider from './contexts/AuthContext'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 import './styles/App.css'
+import User from './types/User'
 
 function App() {
+
+	const [ user, setUser ] = useState<User | null>(null)
+
+	useEffect(() => {
+		const authString = localStorage.getItem('auth')
+		if (authString) {
+			const _user: User = JSON.parse(authString)
+			if(_user){
+				setUser(_user)
+			}
+		}
+	}, [])
+	
+
 	return (
-		<div>
-			Hello World!
-		</div>
+		<AuthProvider value={{ user, setUser }} >
+			<Router>
+				<Routes>
+					<Route path="/" element={<HomePage/>} />
+					<Route path="/login" element={ user ? <HomePage /> : <LoginPage/> } />
+				</Routes>
+			</Router>
+		</AuthProvider>
 	)
 }
 
