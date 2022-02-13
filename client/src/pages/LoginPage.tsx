40438fb3bc3c8923c71b40616aa618e02b378776
Deekshant Yadav <d33kshant@gmail.com>
2022-02-13 13:39:54 +0530

    Add login function for auth

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,10 +1,47 @@
+import { useState } from "react"
+import { useAuthContext } from "../contexts/AuthContext"
+import LoginFormState from "../types/LoginFormState"
+
 const LoginPage: React.FC = () => {
+
+	const [ formState, setFormState ] = useState<LoginFormState>({ username: "", password: "" })
+	const { setUser } = useAuthContext()
+
+	const onFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		fetch('/api/auth/login', {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(formState),
+		}).then(res=>res.json()).then(res=>{
+			if (res.error) {
+				return alert('Failed to login.')
+			}
+			setUser(res)
+			localStorage.setItem('jwt-token', res.token)
+		})
+	}
+
+	const onFormStateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		switch (event.target.name) {
+			case 'username':
+				setFormState({ ...formState, username: event.target.value })
+				break;
+			case 'password':
+				setFormState({ ...formState, password: event.target.value})
+				break;
+		}
+	}
+
 	return (
 		<div>
-			<form>
-				<input type="text" placeholder="Username" />
-				<input type="password" placeholder="Password" />
+			<form onSubmit={onFormSubmit}>
+				<input value={formState.username} onChange={onFormStateChange} autoComplete="off" name="username" type="text" placeholder="Username" />
+				<input value={formState.password} onChange={onFormStateChange} name="password" type="password" placeholder="Password" />
 				<button type="submit">Login</button>
+				<br /><span>Don't have an account? <a href="/signup">Sign Up</a></span>
 			</form>
 		</div>
 	)
