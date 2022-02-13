181719ca97b0aee79e1e19c2104ce89ed5bddeb3
Deekshant Yadav <d33kshant@gmail.com>
2022-02-13 13:36:49 +0530

    Add route for verifying token

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
-const { login, signup } = require('../controllers/AuthController')
+const { login, signup, verify } = require('../controllers/AuthController')
 
+router.get('/verify', verify)
 router.post('/signup', signup)
-
 router.post('/login', login)
 
 module.exports = router
