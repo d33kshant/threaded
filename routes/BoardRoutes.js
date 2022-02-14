04ffdc8a21c4cfb423371d501723e2edf09a6dba
Deekshant Yadav <d33kshant@gmail.com>
2022-02-14 11:50:16 +0530

    Add route to join board

--- routes/BoardRoutes.js
+++ routes/BoardRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const { createBoard, getBoard } = require('../controllers/BoardController')
+const { createBoard, getBoard, joinBoard } = require('../controllers/BoardController')
 const { authenticate, authorize } = require('../controllers/AuthController')
 
 router.post('/create', authenticate, createBoard)
+router.post('/join', authenticate, joinBoard)
 router.get('/:name', authorize, getBoard)
 
 module.exports = router
