8076a375662f0c72923a431ecda3cc8173f9d634
Deekshant Yadav <d33kshant@gmail.com>
2022-02-14 11:49:54 +0530

    Get name from param and check if board exist

--- controllers/BoardController.js
+++ controllers/BoardController.js
@@ -3,11 +3,18 @@ const User = require('../models/User')
 
 const getBoard = async (req, res) => {
 	const username = req.user?.username
-	const { name } = req.body
+	const { name } = req.params
 
 	try {
 		if (name) {
 			const board = await Board.findOne({ name }, { _id: 0, __v: 0 })
+
+			if(!board) {
+				return res.json({
+					error: "Board not found."
+				})
+			}
+
 			const members = await User.find({ board: name }).count()
 			const joined = await User.find({ username, boards: name }).count() !== 0
 			res.json({
@@ -72,4 +79,73 @@ const createBoard = async (req, res) => {
 	}
 }
 
-module.exports = { getBoard, createBoard }
+const joinBoard = async (req, res) => {
+	const { username, id } = req.user
+	const { name: board } = req.body
+
+	if (!username) {
+		return res.status(401).json({
+			error: "Failed to authenticate."
+		})
+	}
+
+	if (!board) {
+		return res.json({
+			error: "Board `name` missing in requeest."
+		})
+	}
+
+	try {
+		const user = await User.findById(id)
+		const _board = await Board.findOne({ name: board })
+
+		if(!_board) {
+			return res.json({
+				error: "Board not found."
+			})
+		}
+
+		if (board) {
+			if (user.boards.indexOf(board) !== -1) {
+				user.boards = user.boards.filter(joined=>joined!==board)
+				user.save((error, result) => {
+					if (!error) {
+						res.json({
+							joined: false,
+							message: `Successfully left ${board}.`
+						})
+					} else {
+						res.json({
+							error: `Failed to leave ${board}.`
+						})
+					}
+				})
+			} else {
+				user.boards.push(board)
+				user.save((error, result) => {
+					if (!error) {
+						res.json({
+							joined: true,
+							message: `Successfully joined ${board}.`
+						})
+					} else {
+						res.json({
+							error: `Failed to join ${board}.`
+						})
+					}
+				})
+			}
+		} else {
+			return res.status(404).json({
+				error: "Board not found."
+			})
+		}
+
+	} catch(error) {
+		res.json({
+			error: "Something went wrong, Try again later."
+		})
+	}
+}
+
+module.exports = { getBoard, createBoard, joinBoard }
