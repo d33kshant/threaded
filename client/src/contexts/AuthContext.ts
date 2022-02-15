089c27106231d882d77bcaba1fa87d13c65a989b
Deekshant Yadav <d33kshant@gmail.com>
2022-02-15 09:47:31 +0530

    Add login and logout function

--- client/src/contexts/AuthContext.ts
+++ client/src/contexts/AuthContext.ts
@@ -1,7 +1,8 @@
 import { createContext, useContext } from 'react'
 import AuthContextInterface from '../types/AuthContext'
+import User from '../types/User'
 
-export const AuthContext = createContext<AuthContextInterface>({ user: null, setUser: (user)=>{} })
+export const AuthContext = createContext<AuthContextInterface>({ user: null, login: (user: User)=>{}, logout: ()=>{} })
 export const useAuthContext = () => useContext(AuthContext)
 const AuthProvider = AuthContext.Provider
 export default AuthProvider
