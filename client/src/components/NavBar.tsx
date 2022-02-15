d0c4fcb72ebcaf45eb9d2c753ab76c1baff1c943
Deekshant Yadav <d33kshant@gmail.com>
2022-02-15 09:45:59 +0530

    Update NavaBar look

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,33 +4,47 @@ import { useAuthContext } from "../contexts/AuthContext"
 
 const NavBar = () => {
 
-	const { user } = useAuthContext()
+	const { user, logout } = useAuthContext()
 
 	return (
-		<Nav>
-			<NavLogo href="/">threaded</NavLogo>
-			{ 
-				user ? 
-				<NavLink to={`/user/${user.username}`}>Account</NavLink> : 
-				<NavLinks>
-					<NavLink to="/signup">Sign-Up</NavLink>
-					<NavLink to="/login">Login</NavLink>
-				</NavLinks>
-			}
-		</Nav>
+		<Container>
+			<Nav>
+				<NavLogo href="/">Threaded</NavLogo>
+				{ 
+					user ? 
+					<NavLinks>
+						<NavLink to={`/user/${user.username}`}>
+							<img src={user.avatar} height={24} style={{borderRadius: "50%"}} />
+						</NavLink>
+						{/* <NavLink onClick={logout} to="/">Log-Out</NavLink> */}
+					</NavLinks> : 
+					<NavLinks>
+						<NavLink to="/signup">Sign-Up</NavLink>
+						<NavLink to="/login">Login</NavLink>
+					</NavLinks>
+				}
+			</Nav>
+		</Container>
 	)
 }
 
 export default NavBar
 
+const Container = styled.header`
+	display: flex;
+	justify-content: center;
+	border-bottom: 1px solid lightgray;
+	background: white;
+`
+
 const Nav = styled.nav`
 	width: 100%;
+	max-width: 750px;
 	padding: 8px 16px;
 	box-sizing: border-box;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-	border-bottom: 1px solid lightgray;
 `
 
 const NavLogo = styled.a`
