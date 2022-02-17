312a84778a489cd697e0e6e3349c83e44a91f3e0
Deekshant Yadav <d33kshant@gmail.com>
2022-02-17 11:36:02 +0530

    Structure the board page

--- client/src/pages/BoardPage.tsx
+++ client/src/pages/BoardPage.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import styled from "styled-components"
+import NavBar from "../components/NavBar"
 import { useAuthContext } from "../contexts/AuthContext"
 import Board from "../types/Board"
+import BoardPosts from "../components/BoardPosts"
 
 const BoardPage = () => {
 	const [board, setBoard] = useState<Board | null>(null)
@@ -24,7 +27,7 @@ const BoardPage = () => {
 		}
 	}, [name])
 
-	const onJoinButtonClick = () => {
+	const onActionButtonClick = () => {
 		fetch('/api/board/join', {
 			method: "POST",
 			headers: {
@@ -56,11 +59,91 @@ const BoardPage = () => {
 	}
 
 	return (
-		<div>
-			<p>{board.name}<br />Members: {board.members}<br /> Created: {(new Date(board.created)).toDateString()}
-			<br />Creator: <a href={`/user/${board.creator}`}>@{board.creator}</a><br /><button onClick={onJoinButtonClick}>{joined ? "Leave Board" : "Join Board"}</button></p>
-		</div>
+		<>
+		<NavBar />
+		<Container>
+			<BannerImage src={board.banner} alt="banner" />
+			<BoardInfo>
+				<BoardIcon src={board.icon} />
+				<BoardName>{board.name}</BoardName>
+				<BoardAbout>{board.about}</BoardAbout>
+				<BoardStats>
+					<svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+					{board.members} • 
+					<svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+					{(new Date(board.created)).toLocaleDateString()}
+				</BoardStats>
+				<BoardActionButton onClick={onActionButtonClick}>{ joined ? "Leave Board" : "Join Board"}</BoardActionButton>
+			</BoardInfo>
+			<BoardPosts board={board.name} />
+		</Container>
+		</>
 	)
 }
 
 export default BoardPage
+
+const Container = styled.div`
+	margin: 0 auto;
+	width: 100%;
+	max-width: 750px;
+	padding: 8px 16px;
+	box-sizing: border-box;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	position: relative;
+`
+
+const BoardInfo = styled.div`
+	width: calc(100% - 32px);
+	display: flex;
+	padding: 8px 16px;
+	box-sizing: border-box;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	/* background: red; */
+	/* position: absolute; */
+	/* top: 116px; */
+`
+
+const BannerImage = styled.img`
+	/* width: 100%; */
+	height: 150px;
+	object-fit: cover;
+	border-radius: 4px;
+	/* position: absolute;
+	z-index: -5; */
+	/* top: 0; */
+`
+
+const BoardIcon = styled.img`
+	border-radius: 8px;
+	width: 64px;
+	height: 64px;
+	border: 4px solid whitesmoke;
+	/* position: absolute; */
+	/* top: calc(75%); */
+	/* left: 16px; */
+`
+
+const BoardName = styled.p`
+	margin: 0;
+	font-weight: 600;
+	font-size: 18px;
+	/* position: absolute; */
+`
+
+const BoardAbout = styled.span``
+
+const BoardStats = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 8px;
+	margin: 8px 0;
+`
+
+const BoardActionButton = styled.button`
+	margin-top: 16px;
+`
