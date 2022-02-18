415e6a294562058897702906cfa936dee1e1cf47
Deekshant Yadav <d33kshant@gmail.com>
2022-02-18 12:39:17 +0530

    Add like feature

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,9 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import PostProps from "../types/PostProps"
 import moment from "moment"
 
-const Post: React.FC<PostProps> = ({ data: { author, body, time, liked, likes, replies } })=> {
+const Post: React.FC<PostProps> = ({ data: { _id, author, body, time, liked: intialLiked, likes, replies } })=> {
+	const [liked, setLiked] = useState<boolean>(intialLiked)
+
+	const onLikeButtonClick = () => {
+		fetch(`/api/post/like`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ id: _id })
+		}).then(res=>res.json()).then(res=>{
+			if (res.error) {
+				return alert(res.error)
+			}
+			setLiked(res.liked)
+		})
+	}
+
 	return (
 		<Container>
 			<PostHeader>
@@ -12,17 +29,19 @@ const Post: React.FC<PostProps> = ({ data: { author, body, time, liked, likes, r
 			</PostHeader>
 			<PostBody>{body}</PostBody>
 			<PostFooter>
-				<PostAction>
-				{	
-					liked ?
-					<svg width="16" height="16" fill="red" viewBox="0 0 24 24"><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12z"></path></g></svg>:
-					<svg width="16" height="16" fill="gray" viewBox="0 0 24 24"><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path></g></svg>
-				}
+				<PostAction onClick={onLikeButtonClick}>
+					{	
+						liked ?
+						<svg width="16" height="16" fill="red" viewBox="0 0 24 24"><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12z"></path></g></svg>:
+						<svg width="16" height="16" fill="gray" viewBox="0 0 24 24"><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path></g></svg>
+					}
+					{liked ? likes : (likes - 1 > 0) ? likes - 1 : ""}
 				</PostAction>
 				<PostAction>
 					{
 						<svg width="16" height="16" viewBox="0 0 24 24"><g><path d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"></path></g></svg>
 					}
+					{ replies > 0 && replies }
 				</PostAction>
 			</PostFooter>
 		</Container>
@@ -81,8 +100,9 @@ const PostFooter = styled.div`
 const PostAction = styled.button`
 	display: flex;
 	align-items: center;
-	background: rgba(0, 0, 0, 0.1);
+	background: none;
 	border: none;
 	padding: 4px 8px;
 	cursor: pointer;
+	gap: 4px;
 `
