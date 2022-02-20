dfa0e7caf1c0629eddc810294f0fbdb1d9231ae9
Deekshant Yadav <d33kshant@gmail.com>
2022-02-20 16:14:48 +0530

    Create Button component

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+const Button = styled.button`
+margin-top: 16px;
+	padding: 8px 16px;
+	background: #eff3f4;
+	border: none;
+	border-radius: 64px;
+	color: black;
+	font-family: inherit;
+	font-weight: bold;
+	cursor: pointer;
+`
+export default Button
