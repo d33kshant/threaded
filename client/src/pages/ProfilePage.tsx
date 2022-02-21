f70947ece63381b1ddb5eb9479c0e49009090d66
Deekshant Yadav <d33kshant@gmail.com>
2022-02-21 14:50:47 +0530

    Update elements and structure

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import styled from "styled-components"
 import NavBar from "../components/NavBar"
-import ActionButton from "../components/Button"
+import Button from "../components/Button"
 import { useAuthContext } from "../contexts/AuthContext"
 import UserProfile from "../types/UserProfile"
 
 const ProfilePage = () => {
 	const { username } = useParams()
-	const {user: currentUser} = useAuthContext()
+	const {user: currentUser, logout} = useAuthContext()
 	const [user, setUser] = useState<UserProfile | null>(null)
 	const [loading, setLoading] = useState<boolean>(true)
 
@@ -18,7 +18,6 @@ const ProfilePage = () => {
 			if(res.error) {
 				return alert(res.error)
 			}
-			console.log(res)
 			setUser(res)
 			setLoading(false)
 		})
@@ -32,10 +31,17 @@ const ProfilePage = () => {
 			{(!loading && !user) && "User not found"}
 			{(!loading && user) &&
 				<>
+				<Header>
 				<UserAvatar src={`http://localhost:3000/${user.avatar}`} alt={user.username} />
+				{ (user.username === currentUser?.username) ? 
+					<ProfileActions>
+						<EditProfileButton>Edit Profile</EditProfileButton>
+						<LogOutButton onClick={logout}>Log Out</LogOutButton>
+					</ProfileActions> 
+				: <Button>{user.inFollowing ? "Unfollow" : "Follow"}</Button>}
+				</Header>
 				<UserName>{user.username}</UserName>
 				{ user.bio && <UserBio>{user.bio}</UserBio>}
-				{ (user.username === currentUser?.username) ? <ActionButton>Edit Profile</ActionButton> : <ActionButton>{user.inFollowing ? "Unfollow" : "Follow"}</ActionButton>}
 				</>
 			}
 		</Container>
@@ -53,12 +59,13 @@ const Container = styled.div`
 	margin: 0 auto;
 	display: flex;
 	flex-direction: column;
+	gap: 8px;
 `
 
 const UserAvatar = styled.img`
 	border-radius: 50%;
-	height: 128px;
-	width: 128px;
+	height: 64px;
+	width: 64px;
 `
 
 const UserName = styled.p`
@@ -68,4 +75,26 @@ const UserName = styled.p`
 
 const UserBio = styled.p`
 	margin: 0;
+	color: #6e767d;
+`
+
+const Header = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`
+
+const ProfileActions = styled.div`
+	display: flex;
+	gap: 8px;
+`
+
+const EditProfileButton = styled(Button)`
+	/* background: transparent;
+	border: 1px solid #eff3f4;
+	color: #eff3f4; */
+`
+
+const LogOutButton = styled(Button)`
+	background: #dd2e44;
 `
