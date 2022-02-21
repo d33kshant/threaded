0dc4739846156bca3a3536fec8946d4b146cc424
Deekshant Yadav <d33kshant@gmail.com>
2022-02-21 14:52:11 +0530

    Add form for new post

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,17 +1,27 @@
-// import { useAuthContext } from "../contexts/AuthContext"
-
+import React, { useRef } from "react"
 import styled from "styled-components"
+import Button from "../components/Button"
 import NavBar from "../components/NavBar"
 
 const HomePage: React.FC = () => {
 	
-	// const { user } = useAuthContext()
-	
+	const inputRef = useRef<HTMLDivElement>(null)
+
+	const createNewPost = () => {
+		alert(inputRef.current?.innerText)
+		if (inputRef.current){
+			inputRef.current.innerText = ''
+		}
+	}
+
 	return (
 		<>
 		<NavBar />
 		<Container>
-			Home Page	
+			<NewPost>
+				<NewPostInput ref={inputRef} contentEditable={true} placeholder="What's happening ?" />
+				<PostButton onClick={createNewPost}>Post</PostButton>
+			</NewPost>	
 		</Container>
 		</>
 	)
@@ -23,8 +33,46 @@ const Container = styled.div`
 	margin: 0 auto;
 	width: 100%;
 	max-width: 750px;
-	padding: 8px 16px;
 	box-sizing: border-box;
 	background: rgba(0, 0, 0, 0.3);
 	height: 100%;
+	border: 1px solid #2F3336;
+	border-top: none;
+	border-bottom: none;
+`
+
+const NewPost = styled.div`
+	border-bottom: 1px solid #2F3336;
+	padding: 8px 16px;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	align-items: flex-end;
+`
+
+const NewPostInput = styled.div`
+	width: 100%;
+	border: none;
+	background: none;
+	resize: vertical;
+	font-family: inherit;
+	font-size: 16px;
+	outline: none;
+	cursor: text;
+
+	:empty::before {
+		content: attr(placeholder);
+		color: gray;
+	}
+`
+
+const PostButton = styled(Button)`
+	background: #1d9bf0;
+	color: white;
+	margin: 4px 0;
+
+	:disabled {
+		opacity: 0.5;
+	}
 `
