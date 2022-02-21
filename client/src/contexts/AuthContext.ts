6d5a5ff65ad00a05504bd5e6e7cd4003925acb2e
Deekshant Yadav <d33kshant@gmail.com>
2022-02-21 20:06:51 +0530

    Remove unused export

--- client/src/contexts/AuthContext.ts
+++ client/src/contexts/AuthContext.ts
@@ -2,7 +2,7 @@ import { createContext, useContext } from 'react'
 import AuthContextInterface from '../types/AuthContext'
 import User from '../types/User'
 
-export const AuthContext = createContext<AuthContextInterface>({ user: null, login: (user: User)=>{}, logout: ()=>{} })
+const AuthContext = createContext<AuthContextInterface>({ user: null, login: (user: User)=>{}, logout: ()=>{} })
 export const useAuthContext = () => useContext(AuthContext)
 const AuthProvider = AuthContext.Provider
 export default AuthProvider
