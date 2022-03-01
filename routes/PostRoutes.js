6cc0eabe7d74229f56f67248ed481a10c9a2e95a
Deekshant Yadav <d33kshant@gmail.com>
2022-03-01 23:44:01 +0530

    Add route to get feed posts

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { getPost, getPosts, createPost, likePost, getReplies, deletePost } = require('../controllers/PostController')
+const { getPost, getPosts, createPost, likePost, getReplies, deletePost, getFeed } = require('../controllers/PostController')
 const { authenticate, authorize } = require('../controllers/AuthController')
 
 router.post('/create', authenticate, createPost)
@@ -7,6 +7,7 @@ router.post('/like', authenticate, likePost)
 router.post('/delete', authenticate, deletePost)
 router.get('/:id/replies', authorize, getReplies)
 router.get('/:id', authorize, getPost)
+router.get('/feed', authorize, getFeed)
 router.get('/', authorize, getPosts)
 
 module.exports = router
