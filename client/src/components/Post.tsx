6f6d0424d9346d09670316035a78a9b96a5c9c0a
Deekshant Yadav <d33kshant@gmail.com>
2022-03-03 00:00:01 +0530

    Add board name in post header

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -8,7 +8,7 @@ import PostReplyModal from "./PostReplyModal"
 import { useToastContext } from "../contexts/ToastContext"
 import { MessageType } from "../types/Message"
 
-const Post: React.FC<PostProps> = ({ isReply, replyable, data: { _id, author, body, time, liked: intialLiked, likes: initialLikes, replies, ref } })=> {
+const Post: React.FC<PostProps> = ({ isReply, replyable, data: { _id, author, body, board, time, liked: intialLiked, likes: initialLikes, replies, ref } })=> {
 	const [liked, setLiked] = useState<boolean>(intialLiked)
 	const [likes, setLikes] = useState<number>(initialLikes)
 	const [replyModal, setReplyModal] = useState<boolean>(false)
@@ -56,7 +56,8 @@ const Post: React.FC<PostProps> = ({ isReply, replyable, data: { _id, author, bo
 			<PostHeader>
 				{ isReply && <svg width="16" height="16" fill="#00ba7c" viewBox="0 0 24 24"><g><path d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"></path></g></svg>}
 				<PostAuthor>{author}</PostAuthor> •
-				<PostTime>{moment(time).fromNow()}</PostTime>
+				<PostTime>{moment(time).fromNow()} in </PostTime>
+				<PostBoard href={`/board/${board}`} >{board}</PostBoard>
 			</PostHeader>
 			<PostBody>{body}</PostBody>
 			<PostFooter>
@@ -115,13 +116,25 @@ const PostHeader = styled.div`
 
 const PostAuthor = styled.p`
 	text-decoration: none;
-	/* color: black; */
 	font-weight: 600;
 	margin: 0;
+
+	:hover {
+		text-decoration: underline;
+	}
 `
 
 const PostTime = styled.span`
 	color: #6e767d;
+`
+
+const PostBoard = styled.a`
+	color: #6e767d;
+	text-decoration: none;
+
+	:hover {
+		text-decoration: underline;
+	}
 `
 
 const PostBody = styled.p`
